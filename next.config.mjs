85614f2af4ccd489a758cbb3f49ec877eeaa7902
Wael Feriz <wael.sam42@gmail.com>
2026-01-21 22:21:21 +0100

    fix: use env var for basePath in GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
-const isProd = process.env.NODE_ENV === 'production'
+const basePathValue = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   transpilePackages: ["next-image-export-optimizer"],
-  basePath: isProd ? '/Website' : '',
-  assetPrefix: isProd ? '/Website' : '',
+  basePath: basePathValue,
+  assetPrefix: basePathValue,
   images: {
     loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '/Website' : '',
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: "75",
